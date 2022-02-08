3e708279c516d5de1fe5c3f0af5d7c1c7a467ed8
david <davleop1999@gmail.com>
2022-02-08 17:13:22 -0600

    more bootstrap...

--- src/renderer/bottombar.tsx
+++ src/renderer/bottombar.tsx
@@ -1,13 +1,34 @@
 import { PureComponent } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import './bottombar.css';
 
 class BottomBar extends PureComponent {
   render() {
     return (
       <div id="botbar">
-        <div className="playpause">
-          <input type="checkbox" value="None" id="playpause" name="check" />
-          <label htmlFor="playpause" tabIndex={1} />
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm">
+              <button>test</button>
+            </div>
+            <div className="col-sm">
+              <div className="playpause">
+                <input type="checkbox" value="None" id="playpause" name="check" />
+                <label htmlFor="playpause" tabIndex={1} />
+              </div>
+            </div>
+            <div className="col-sm"></div>
+          </div>
+
+          <div className="row">
+            <div className="col-sm"></div>
+            <div className="col-sm">
+              <div className="timeline">
+                <input type="range" min="0" max="100" />
+              </div>
+            </div>
+            <div className="col-sm"></div>
+          </div>
         </div>
       </div>
     );
