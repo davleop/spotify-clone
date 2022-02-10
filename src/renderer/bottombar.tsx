18a8029eb2b2d64d423af6a0759f244ca6379c38
david <davleop1999@gmail.com>
2022-02-09 19:59:27 -0600

    scrub bar almost complete

--- src/renderer/bottombar.tsx
+++ src/renderer/bottombar.tsx
@@ -1,8 +1,28 @@
-import { PureComponent } from 'react';
+import { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './bottombar.css';
 
-class BottomBar extends PureComponent {
+class BottomBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+    const val = event.target.value;
+    const { min } = event.target;
+    const { max } = event.target;
+    const sz = ((val - min) * 100) / (max - min);
+    event.target.style.backgroundSize = `${sz}% 100%`;
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div id="botbar">
@@ -22,7 +42,14 @@ class BottomBar extends PureComponent {
             <div className="col-sm"></div>
             <div className="col-sm">
               <div className="timeline">
-                <input id="rangevalue" type="range" min="0" max="100" />
+                <input
+                  id="rangevalue"
+                  type="range"
+                  min="0"
+                  max="100"
+                  onChange={this.handleChange}
+                  defaultValue="0"
+                />
               </div>
             </div>
             <div className="col-sm"></div>
