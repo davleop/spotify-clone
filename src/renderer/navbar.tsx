1e9ac777bc2547152e49108af93172e5e85163ca
david <davleop1999@gmail.com>
2022-02-13 18:43:37 -0600

    add account dropbox

--- src/renderer/navbar.tsx
+++ src/renderer/navbar.tsx
@@ -5,8 +5,18 @@ class MainNavBar extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      isOpen: false,
+    };
+
     this.handlePageForward = this.handlePageForward.bind(this);
     this.handlePageBackward = this.handlePageBackward.bind(this);
+    this.toggleOpen = this.toggleOpen.bind(this);
+  }
+
+  // eslint-disable-next-line react/sort-comp
+  toggleOpen(_event: unknown) {
+    this.setState({ isOpen: !this.state.isOpen });
   }
 
   handlePageForward(event) {
@@ -18,18 +28,18 @@ class MainNavBar extends Component {
   }
 
   render() {
+    const menuClass = `account-dropbox dropdown-menu${this.state.isOpen ? " show" : ""}`;
     return (
       <div id="nav">
         <div className="row">
           <div className="col-sm-1" />
-          <div className="col-sm-1" />
-          <div className="col-sm-3">
+          <div className="col-sm-2">
             <button
               aria-label="PageBack"
               id="page-back"
               type="button"
               onClick={this.handlePageBackward}
-              className="bi bi-arrow-left-circle-fill d-flex justify-content-center align-items-center back-page"
+              className="bi bi-arrow-left-circle d-flex justify-content-center align-items-center back-page"
             />
           </div>
           <div className="col-sm-2">
@@ -38,10 +48,53 @@ class MainNavBar extends Component {
               id="page-forward"
               type="button"
               onClick={this.handlePageForward}
-              className="bi bi-arrow-right-circle-fill d-flex justify-content-center align-items-center back-page"
+              className="bi bi-arrow-right-circle d-flex justify-content-center align-items-center back-page"
             />
           </div>
-          <div className="col-sm" />
+          <div className="col-sm">
+            <div className="dropdown">
+              <button
+                id="account-dropdown"
+                onClick={this.toggleOpen}
+                type="button"
+                className="btn dropdown-toggle"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <span className="bi bi-person-circle account-icon" />
+              </button>
+              <ul className={menuClass} aria-labelledby="account-dropdown">
+                <li>
+                  <a className="dropdown-item" href="/">
+                    Account
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/">
+                    Profile
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/">
+                    Private session
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/">
+                    Settings
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/">
+                    Log out
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
 
         <a href="/">
